008f97ac93620a761a1dea13dcce70174dd46f8b
Swarnabha Sinha <swarnabhasinha.201mn059@nitk.edu.in>
2023-05-04 11:47:06 +0530

    feat: frontend setup and work

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import ThirdwebGuideFooter from "../components/guide/ThirdwebGuideFooter";
+import "./styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// This is the chain your dApp will work on.
+const activeChain = "goerli";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+      <ThirdwebProvider activeChain={activeChain}>
+        <Head>
+          <title>thirdweb Signature Based Minting</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta
+              name="description"
+              content="thirdweb Example Repository to Showcase signature based minting on an NFT Collection contract"
+          />
+          <meta name="keywords" content="thirdweb signature based minting" />
+        </Head>
+        <Component {...pageProps} />
+        <ThirdwebGuideFooter />
+      </ThirdwebProvider>
+  );
 }
+
+export default MyApp;
--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,118 +1,153 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import styles from "./styles/Home.module.css";
+import {
+  ThirdwebNftMedia,
+  useAddress,
+  useContract,
+  useNFTs,
+  Web3Button,
+} from "@thirdweb-dev/react";
+import type { NextPage } from "next";
+import { useState } from "react";
+import { NFT_COLLECTION_ADDRESS } from '../const/yourDetails';
 
-const inter = Inter({ subsets: ['latin'] })
+const Home: NextPage = () => {
+  const address = useAddress();
 
-export default function Home() {
+  // Fetch the NFT collection from thirdweb via it's contract address.
+  const { contract: nftCollection } = useContract(
+      NFT_COLLECTION_ADDRESS,
+      "nft-collection"
+  );
+
+
+  // Load all the minted NFTs in the collection
+  const { data: nfts, isLoading: loadingNfts } = useNFTs(nftCollection);
+
+  // Here we store the user inputs for their NFT.
+  const [nftName, setNftName] = useState<string>("");
+
+  // This function calls a Next JS API route that mints an NFT with signature-based minting.
+  // We send in the address of the current user, and the text they entered as part of the request.
+  const mintWithSignature = async () => {
+    try {
+      // Make a request to /api/server
+      const signedPayloadReq = await fetch(`/api/server`, {
+        method: "POST",
+        body: JSON.stringify({
+          authorAddress: address, // Address of the current user
+          nftName: nftName || "",
+        }),
+      });
+
+      // Grab the JSON from the response
+      const json = await signedPayloadReq.json();
+
+      if (!signedPayloadReq.ok) {
+        alert(json.error);
+      }
+
+      // If the request succeeded, we'll get the signed payload from the response.
+      // The API should come back with a JSON object containing a field called signedPayload.
+      // This line of code will parse the response and store it in a variable called signedPayload.
+      const signedPayload = json.signedPayload;
+
+      // Now we can call signature.mint and pass in the signed payload that we received from the server.
+      // This means we provided a signature for the user to mint an NFT with.
+      const nft = await nftCollection?.signature.mint(signedPayload);
+
+      alert("Minted succesfully!");
+
+      return nft;
+    } catch (e) {
+      console.error("An error occurred trying to mint the NFT:", e);
+    }
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
+      <div className={styles.container}>
+        <h1 className={styles.h1}>Signature-Based Minting</h1>
+        <p className={styles.explain}>
+          Signature-based minting with{" "}
+          <b>
+            {" "}
+            <a
+                href="https://thirdweb.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.purple}
+            >
+              thirdweb
+            </a>
+          </b>{" "}
+          + Next.JS to create a community-made NFT collection with restrictions.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+
+        <p>
+          Hint: We only generate signatures if your NFT name is a cool{" "}
+          <b>animal name</b>! 😉
+        </p>
+
+        <hr className={styles.divider} />
+
+        <div className={styles.collectionContainer}>
+          <h2 className={styles.ourCollection}>
+            Mint your own NFT into the collection:
+          </h2>
+
+          <input
+              type="text"
+              placeholder="Name of your NFT"
+              className={styles.textInput}
+              maxLength={26}
+              onChange={(e) => setNftName(e.target.value)}
+          />
+        </div>
+
+        <div style={{ marginTop: 24 }}>
+          <Web3Button
+              contractAddress={process.env.NEXT_PUBLIC_NFT_COLLECTION_ADDRESS!}
+              action={() => mintWithSignature()}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Mint NFT
+          </Web3Button>
+        </div>
+
+        <hr className={styles.smallDivider} />
+
+        <div className={styles.collectionContainer}>
+          <h2 className={styles.ourCollection}>Other NFTs in this collection:</h2>
+
+          {loadingNfts ? (
+              <p>Loading...</p>
+          ) : (
+              <div className={styles.nftGrid}>
+                {nfts?.map((nft) => (
+                    <div className={styles.nftItem} key={nft.metadata.id.toString()}>
+                      <div style={{ textAlign: "center" }}>
+                        <p>Name</p>
+                        <p>
+                          <b>{nft.metadata.name}</b>
+                        </p>
+                      </div>
+
+                      <div style={{ textAlign: "center" }}>
+                        <p>Owned by</p>
+                        <p>
+                          <b>
+                            {nft.owner
+                                .slice(0, 6)
+                                .concat("...")
+                                .concat(nft.owner.slice(-4))}
+                          </b>
+                        </p>
+                      </div>
+                    </div>
+                ))}
+              </div>
+          )}
         </div>
       </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
